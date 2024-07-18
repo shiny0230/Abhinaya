df9319dee8146f9461a6851fd84e78f6f7147f19
shiny0230 <shinygreddy2005@gmail.com>
2024-07-18 14:06:27 +0530

    commiting the changes to local repo

---
+++ p1.py
@@ -0,0 +1,9 @@
+def main():
+    # Prompting the user for their name
+    name = input("Enter your name: ")
+    
+    # Greeting the user
+    print(f"Hello, {name}! Welcome to Python programming.")
+
+if __name__ == "__main__":
+    main()
